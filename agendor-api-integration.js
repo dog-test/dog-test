be6a560478f4e97435dc1a00f99bd2b57afa06ae
Rodrigo <rodfeal@gmail.com>
2017-02-07 15:12:57 -0200

    Fix some issues

--- agendor-api-integration.js
+++ agendor-api-integration.js
@@ -19,7 +19,7 @@ module.exports.nextTask = function nextTask(sendTextMessage, sender, res) {
     console.log(JSON.parse(body)[0]);
     console.log("end statusCode");
     if (!error && response.statusCode == 200) {
-      var text = formatTask(body);
+      var text = formatTask(JSON.parse(body)[0]);
       sendTextMessage(text, sender);
       res.sendStatus(200);
     }
